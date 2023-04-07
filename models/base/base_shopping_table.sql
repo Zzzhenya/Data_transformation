eea9583e658cbeb0f258a86813480df76339a40f
Shenya De Silva <shenya44@outlook.com>
2023-04-08 00:59:36 +0200

    Limit 100 update without matrialize function

--- models/base/base_shopping_table.sql
+++ models/base/base_shopping_table.sql
@@ -1,14 +1,20 @@
 
-{{ config(materialized='table') }}
+--{{ config(materialized='table') }}
 
-with source_data as (
 
-    select
-        *,
-        'TRY' as currency,
-    from {{ source('bigquery','raw_shopping_data')}}
+--with source_data as (
 
-)
+--    select
+--        *,
+--        'TRY' as currency,
+--    from {{ source('bigquery','raw_shopping_data')}}
 
-select *
-from source_data
+--)
+
+--select *
+--from source_data
+
+SELECT *
+FROM {{ source('bigquery','raw_shopping_data')}}
+LIMIT 100
+
