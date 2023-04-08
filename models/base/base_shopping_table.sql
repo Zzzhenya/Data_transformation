04e80d4a515b9be2ba8b6e22310ad9c9049c8df1
Shenya De Silva <shenya44@outlook.com>
2023-04-08 22:55:26 +0200

    Reverting to old base model

--- models/base/base_shopping_table.sql
+++ models/base/base_shopping_table.sql
@@ -1,20 +1,16 @@
 
---{{ config(materialized='table') }}
+{{ config(materialized='table') }}
 
 
---with source_data as (
+with source_data as (
 
---    select
---        *,
---        'TRY' as currency,
---    from {{ source('bigquery','raw_shopping_data')}}
+    select
+        *,
+        'TRY' as currency,
+    from {{ source('bigquery','raw_shopping_data')}}
 
---)
+)
 
---select *
---from source_data
-
-SELECT *
-FROM {{ source('bigquery','raw_shopping_data')}}
-LIMIT 100
+select *
+from source_data
 
